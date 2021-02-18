017369d3deec28e10ec45ff0d9893af4a8312999
Henry Velasquez <hvelas93@gmail.com>
2021-02-18 19:41:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,25 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
+def load_library(yamlfile)
   # code goes here
+original_emoticon_NDS=YAML.load_file(yamlfile)
+
+
+# final_results=data.each_with_object({}) do |(key,value), final_array|
+#     #binding.pry
+#     value.each do |inner_key,names|
+#       names.each do |name|
+#         #binding.pry
+#         if !final_array[name]
+#           final_array[name]={}
+
+# original_emoticon_NDS.each_with_object({}) do |(key,value), new_structure|
+binding.pry
+
 end
+
 
 def get_japanese_emoticon
   # code goes here
