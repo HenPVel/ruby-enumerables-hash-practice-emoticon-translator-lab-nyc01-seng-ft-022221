e92c4fd4f348f2053813025af28124e72189fb0c
Henry Velasquez <hvelas93@gmail.com>
2021-02-18 20:44:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,12 +15,7 @@ end
 
 def get_japanese_emoticon(yamlfile,emoticon)
   # code goes here
-original_emoticon_NDS=YAML.load_file(yamlfile)
-
-      original_emoticon_NDS.each_with_object({}) do |(key,value), new_structure|
-      new_structure[key]={english:original_emoticon_NDS[key][0],japanese:original_emoticon_NDS[key][1]}
-      #binding.pry
-      end
+load_library(yamlfile)
 
       binding.pry
 end
