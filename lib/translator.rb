28d1bc251797bb0d646e9bcb61ffd2f181842ee6
Henry Velasquez <hvelas93@gmail.com>
2021-02-18 19:54:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,8 +6,10 @@ def load_library(yamlfile)
   # code goes here
 original_emoticon_NDS=YAML.load_file(yamlfile)
 
-original_emoticon_NDS.each_with_object({}) do |(key,value), new_structure|
-
+    original_emoticon_NDS.each_with_object({}) do |(key,value), new_structure|
+    new_structure[key]=[]  
+    binding.pry
+    end
 # final_results=data.each_with_object({}) do |(key,value), final_array|
 #     #binding.pry
 #     value.each do |inner_key,names|
@@ -15,8 +17,7 @@ original_emoticon_NDS.each_with_object({}) do |(key,value), new_structure|
 #         #binding.pry
 #         if !final_array[name]
 #           final_array[name]={}
-binding.pry
-end
+
 
  #original_emoticon_NDS.each_with_object({}) do |(key,value), new_structure|
 
