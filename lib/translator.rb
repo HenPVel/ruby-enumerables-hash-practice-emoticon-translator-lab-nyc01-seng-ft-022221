7d9ab41901985e7a07d1ca1a801a5cdb030c5516
Henry Velasquez <hvelas93@gmail.com>
2021-02-18 20:02:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(yamlfile)
 original_emoticon_NDS=YAML.load_file(yamlfile)
 
     original_emoticon_NDS.each_with_object({}) do |(key,value), new_structure|
-    new_structure[key]={"english","japanese"}
+    new_structure[key]={"english"=>,"japanese"=>}
     binding.pry
     end
 
